e524653ceead4baed2895b09b7a153d05a7cfc4c
vlad-glivuk <Vladislav.Glivuk@fivesysdev.com>
2023-06-28 23:02:27 +0300

    fix deploy env

--- next.config.js
+++ next.config.js
@@ -2,8 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    loader: 'akamai',
-    path: '',
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
