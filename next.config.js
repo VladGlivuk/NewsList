8a2d8e661946cd4d7c77251ecd031c69ea987090
vlad-glivuk <Vladislav.Glivuk@fivesysdev.com>
2023-06-28 22:53:41 +0300

    fix deploy env

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
-  },
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: 'https',
+  //       hostname: '**',
+  //     },
+  //   ],
+  // },
 };
 
 module.exports = nextConfig;
